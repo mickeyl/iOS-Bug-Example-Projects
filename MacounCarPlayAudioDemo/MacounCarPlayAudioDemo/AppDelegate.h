767003739eee0cd4c1fb606db5b70221e8c7ee7e
Dr. Michael Lauer <mickey@vanille-media.de>
2019-09-11 12:32:18 +0200

    macoun: create car play audio demo project

---
+++ MacounCarPlayAudioDemo/MacounCarPlayAudioDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MacounCarPlayAudioDemo
+//
+//  Created by Dr. Michael Lauer on 11.09.19.
+//  Copyright © 2019 Vanille-Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MacounCarPlayAudioDemo/MacounCarPlayAudioDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MacounCarPlayAudioDemo
+//
+//  Created by Dr. Michael Lauer on 11.09.19.
+//  Copyright © 2019 Vanille-Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
