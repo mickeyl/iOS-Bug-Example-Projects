9080738ed0719b61ef53830bfab5bae2f8510683
Dr. Michael Lauer <mickey@vanille-media.de>
2019-09-15 15:38:34 +0200

    macoun: start w/ nav demo

---
+++ MacounCarPlayNavigationDemo/MacounCarPlayNavigationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MacounCarPlayNavigationDemo
+//
+//  Created by Dr. Michael Lauer on 15.09.19.
+//  Copyright © 2019 Vanille-Media. All rights reserved.
+//
+
+#import <CarPlay/CarPlay.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, CPApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MacounCarPlayNavigationDemo/MacounCarPlayNavigationDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MacounCarPlayNavigationDemo
+//
+//  Created by Dr. Michael Lauer on 15.09.19.
+//  Copyright © 2019 Vanille-Media. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
